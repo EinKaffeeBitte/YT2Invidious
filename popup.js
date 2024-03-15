dfe756e372c822ef804ffee02e2d3e64bb0bfa31
EinKaffeeBitte <163449566+EinKaffeeBitte@users.noreply.github.com>
2024-03-15 09:54:46 +0000

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-// This is your popup script
-document.getElementById('saveButton').onclick = function() {
-    let selectedUrl = document.getElementById('urlSelect').value;
-    // Save the selected URL in chrome.storage.local
-    chrome.storage.local.set({'useURL': selectedUrl}, function() {
-        console.log('URL saved');
-    });
-}
-
